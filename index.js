3c985af0c7179794958db5c62e65e8609d262470
jeremy-rodrguez <jeremy.rodrguez@yahoo.com>
2021-01-17 21:27:08 -0800

    Altering and Inserting HTML

--- index.js
+++ index.js
@@ -1,1 +1,4 @@
-// Write your code here!
+var newHeader = document.createElement('h1');
+newHeader.setAttribute("id","victory");
+newHeader.innerHTML = "JEREMY is the champion";
+document.body.appendChild(newHeader);
