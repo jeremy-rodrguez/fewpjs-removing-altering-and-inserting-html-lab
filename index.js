f413119460df445e43ab855bffa86be25170330a
jeremy-rodrguez <jeremy.rodrguez@yahoo.com>
2021-01-17 21:26:25 -0800

    commit tree 8475c9d57cb8ee9a2d5618d90a794681ac02d130

--- index.js
+++ index.js
@@ -1,1 +1,6 @@
-// Write your code here!
+var newHeader = document.createElement('h1');
+newHeader.setAttribute("id","victory");
+newHeader.innerHTML = "JEREMY is the champion";
+// innerText = document.createTextNode("LUKAAAAA is the champion");
+// newHeader.appendChild(innerText);
+document.body.appendChild(newHeader);
